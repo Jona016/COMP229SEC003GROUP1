ed9f0efe4096c5483f1b681fdee63e42effa6819
Gisella Tavara <gtavaras@my.centennialcollege.ca>
2024-03-23 19:12:21 -0400

    database connected

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 import config from './config/config.js' 
 import app from './server/express.js'
+import mongoose from 'mongoose' 
+mongoose.Promise = global.Promise
+mongoose.connect(config.mongoUri, { 
+    //useNewUrlParser: true,
+//useCreateIndex: true, 
+//useUnifiedTopology: true 
+} ) 
+.then(() => {
+    console.log("Connected to the database!");
+    })
+    
+mongoose.connection.on('error', () => {
+throw new Error(`unable to connect to database: ${config.mongoUri}`) 
+})
 app.get("/", (req, res) => {
 res.json({ message: "Welcome to User application." });
 });
@@ -9,5 +23,3 @@ console.log(err)
 }
 console.info('Server started on port %s.', config.port) 
 })
-
-
